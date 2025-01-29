7022073eeb107236b688403fc92d1130318c1099
O Ala <141387803+allanjose001@users.noreply.github.com>
2025-01-29 19:53:27 -0300

    Adição da pasta controllers

---
+++ BattleshipBack/controllers/boardController.h
@@ -0,0 +1,28 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "board.h"
+#include <QObject>
+
+class Controller : public QObject {
+
+    Q_OBJECT // Necessário para usar sinais e slots do Qt
+    private:
+        Board board; // Instância do tabuleiro.
+    public:
+        Controller(); // Construtor
+        ~Controller(); // Destrutor
+
+    // Eventos recebidos do frontend:
+    void placeShip(int x, int y, Ship& ship); // Posiciona um navio no tabuleiro.
+    void attackPosition(int x, int y);       // Ataca uma posição específica.
+
+    // Dados enviados ao frontend:
+    Position (&getBoardState())[10][10];     // Retorna o estado atual do tabuleiro.
+
+signals:
+    void boardUpdated();  // Sinal emitido quando o tabuleiro é atualizado.
+    void attackResult(bool hit); // Sinal emitido após um ataque (acerto ou erro).
+};
+
+#endif
